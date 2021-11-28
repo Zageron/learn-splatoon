6a888b9f9b0820ec1d1b2273d0c36bdebff3ad68
Zageron <hello@zageron.ca>
2021-11-27 17:03:20 -0800

    Add tests for reading json data

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,17 @@ use actix_web::{dev, get, http, post, web, App, HttpResponse, HttpServer, Result
 
 use handlebars::Handlebars;
 
+use rand::thread_rng;
+use rand::Rng;
+
 use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
 use std::io;
+use std::io::BufReader;
+use std::path::Path;
 
 // Macro documentation can be found in the actix_web_codegen crate
 #[get("/")]
@@ -80,6 +89,54 @@ async fn robots(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
     builder.body(body)
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct LanguageLookup {
+    #[serde(flatten)]
+    inner: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct MainWeapons {
+    addition: u32,
+    id: u32,
+    ink_saver_lv: String,
+    lock: String,
+    main_up_gear_power_type: String,
+    move_vel_lv: String,
+    name: String,
+    param0: String,
+    param1: String,
+    param2: String,
+    param_value0: u32,
+    param_value1: u32,
+    param_value2: u32,
+    price: u32,
+    range: u32,
+    rank: u32,
+    shot_move_vel_type: String,
+    special: String,
+    special_cost: u32,
+    stealth_move_acc_lv: String,
+    sub: String,
+}
+
+fn read_language_file<P: AsRef<Path>>(path: P) -> Result<LanguageLookup, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let language: LanguageLookup =
+        serde_json::from_reader(reader).expect("JSON was not well-formatted.");
+    Ok(language)
+}
+
+fn read_mains_file<P: AsRef<Path>>(path: P) -> Result<Vec<MainWeapons>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mains: Vec<MainWeapons> =
+        serde_json::from_reader(reader).expect("JSON was not well-formatted.");
+    Ok(mains)
+}
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     #[cfg(debug_assertions)]
@@ -124,4 +181,38 @@ fn not_found<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse
         http::HeaderValue::from_static("Error"),
     );
     Ok(ErrorHandlerResponse::Response(res))
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::thread_rng;
+    use rand::Rng;
+
+    use crate::read_language_file;
+    use crate::read_mains_file;
+    use crate::MainWeapons;
+
+    #[test]
+    fn language_english_exists() {
+        assert!(read_language_file("./data/splatoon/english.json").is_ok());
+    }
+
+    #[test]
+    fn mains_exists() {
+        assert!(read_mains_file("./data/splatoon/mains.json").is_ok());
+    }
+
+    #[test]
+    fn test_langauge_lookup() {
+        // Testing out splatoon weaponry stuff.
+        let english_lang = read_language_file("./data/splatoon/english.json").unwrap();
+        let main_weapons = read_mains_file("./data/splatoon/mains.json").unwrap();
+
+        let mut rng = thread_rng();
+        let i: usize = rng.gen_range(0..main_weapons.len());
+
+        let main: &MainWeapons = &main_weapons[i];
+        let name: &String = &english_lang.inner[&main.name];
+        println!("{} -- {:?}", name, main);
+    }
 }
